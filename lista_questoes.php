07141c4bfca8a96ce67a5aff42571e226b5271b7
Eduardo Oliveira <emaildoedu00@gmail.com>
2018-06-21 22:16:44 -0300

    Inserido botao de resposta apenas os que possuem resposta | Inserido resposta 4 com imagem

--- lista_questoes.php
+++ lista_questoes.php
@@ -10,23 +10,23 @@
         ),
         array( 
             "enunciado" => "Em PHP, escreva as classes necessárias para representar um estacionamento.",
-            "botao" => "tres"
+            // "botao" => "tres"
         ),
         array(
             "enunciado" => "Qual o resultado do trecho de código a seguir:",
-            "botao" => "quatro",
             "codigo" => "<span class='funcao'>class</span> <span class='nomeClasse'>W</span> { <br>
                 <p>&emsp;&emsp;<span class='funcao'>const</span> <span class='nomeClasse'>XX</span> = <span class='funcao'>12 + 30</span>;</p>
             } <br>
-            <span class='funcao'>echo</span> W::XX; <br>"
+            <span class='funcao'>echo</span> W::XX; <br>",
+            "botao" => "quatro",
         ),
         array( 
             "enunciado" => "Em NodeJS existem threads?",
-            "botao" => "cinco"
+            // "botao" => "cinco"
         ),
         array( 
             "enunciado" => "Em sudoku, o objetivo é preencher uma grade 9x9 subdivida em quadrantes 3x3 com números de 1 a 9 de tal forma que não hajam números repetidos em uma mesma coluna, linha ou quadrante. Escreva um procedimento que gere uma matriz 9x9 válida como resultado de sudoku considerando uma grade vazia.",
-            "botao" => "seis"
+            // "botao" => "seis"
         ),
         array( 
             "enunciado" => "Escreva um sistema de enquetes que atenda aos seguintes requisitos:",
@@ -36,11 +36,11 @@
                 "Deve ser possível saber qual usuário criou cada enquete",
                 "Todas as enquetes são públicas"
             ),
-            "botao" => "sete"
+            // "botao" => "sete"
         ),
         array( 
             "enunciado" => "Você já conhecia algum dos problemas apresentados nesta avaliação? De onde?",
-            "botao" => "oito"
+            // "botao" => "oito"
         ),
     );
 ?>
---
+++ quatro.php
@@ -0,0 +1,11 @@
+<?php
+// Qual o resultado do trecho de código a seguir
+include_once('load_fontes.php');
+include('voltar.php');
+?>
+
+42
+<br>
+Poderia jurar que era alguma pegadinha, preferi fazer esse código para não falar besteira. Mas é só o sentido da vida mesmo hehehe... captei a referência! ;D
+<br>
+<img src="dontpanic.png" />
